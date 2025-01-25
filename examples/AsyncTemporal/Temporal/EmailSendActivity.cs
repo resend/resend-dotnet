0b543e3f81f5d239468f14d6067373e492d0bbed
Filipe Toscano <filipe.toscano@gmail.com>
2025-01-25 12:16:33 +0400

    Use Temporal exception to set nextRetryDelay

--- examples/AsyncTemporal/Temporal/EmailSendActivity.cs
+++ examples/AsyncTemporal/Temporal/EmailSendActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Resend;
 using Temporalio.Activities;
+using Temporalio.Exceptions;
 
 namespace AsyncTemporal.Temporal;
 
@@ -20,38 +21,60 @@ public class EmailSendActivity
 
     /// <summary />
     [Activity]
-    public async Task<string> EmailSendAsync( EmailMessage email )
+    public async Task<string> EmailSendAsync( EmailMessage message )
     {
+        _logger.LogDebug( "Email to {To}: {Subject}", message.To, message.Subject );
+
+
         /*
          * 
          */
-        if ( email.Attachments?.Count > 0 )
+        if ( message.Attachments?.Count > 0 )
         {
-            foreach ( var att in email.Attachments )
+            foreach ( var att in message.Attachments )
             {
                 if ( att.Path?.StartsWith( "load:" ) == false )
                     continue;
 
                 _logger.LogDebug( "Attachment: Load {Filename} based on {LoadPath}", att.Filename, att.Path );
+                ActivityExecutionContext.Current.Heartbeat();
 
-                att.Content = ContentLoad( att.Path! );
+                att.Content = await ContentLoadAsync( att.Path!, ActivityExecutionContext.Current.CancellationToken );
                 att.Path = null;
             }
         }
 
 
         /*
-         * 
+         * When throwing Temporal's ApplicationFailureException, it is possible to
+         * set nextRetryDelay to inform when to retry the activity.
          */
-        var resp = await _resend.EmailSendAsync( email );
+        ActivityExecutionContext.Current.Heartbeat();
+        ResendResponse<Guid> resp;
+
+        try
+        {
+            resp = await _resend.EmailSendAsync( message, ActivityExecutionContext.Current.CancellationToken );
+        }
+        catch ( ResendException ex )
+        {
+            TimeSpan nextRetryDelay = new TimeSpan( 0, 0, 10 );
+
+            throw new ApplicationFailureException(
+                ex.Message,
+                errorType: ex.ErrorType.ToString(),
+                nextRetryDelay: nextRetryDelay );
+        }
 
         return resp.Content.ToString();
     }
 
 
     /// <summary />
-    private byte[]? ContentLoad( string attachmentPath )
+    private async Task<byte[]> ContentLoadAsync( string attachmentPath, CancellationToken cancellationToken )
     {
+        await Task.Delay( 1_000, cancellationToken );
+
         // TODO: Load based on attachmentPath
 
         throw new NotImplementedException();
