2eee9a95800da7257cd3767637b83d111b8bc5b8
Filipe Toscano <filipe.toscano@gmail.com>
2025-01-25 14:02:27 +0400

    Add data for bounced emails
    Related to #23

--- src/Resend.Webhooks/EmailEventData.cs
+++ src/Resend.Webhooks/EmailEventData.cs
@@ -23,15 +23,43 @@ public class EmailEventData : IWebhookData
     public EmailAddressList To { get; set; } = default!;
 
     /// <summary />
+    [JsonPropertyName( "subject" )]
+    public string Subject { get; set; } = default!;
+
+    /// <summary />
     /// <remarks>
-    /// Only set for <see cref="Resend.WebhookEventType.EmailClicked" />, otherwise is null.
+    /// Only set for <see cref="WebhookEventType.EmailClicked" />, otherwise is null.
     /// </remarks>
     [JsonPropertyName( "click" )]
+    [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
     public EmailClickData? Click { get; set; }
 
     /// <summary />
-    [JsonPropertyName( "subject" )]
-    public string Subject { get; set; } = default!;
+    /// <remarks>
+    /// Only set for <see cref="WebhookEventType.EmailBounced" />, otherwise is null.
+    /// </remarks>
+    [JsonPropertyName( "bounce" )]
+    [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+    public EmailBounceData? Bounce { get; set; }
+}
+
+
+/// <summary />
+public class EmailBounceData
+{
+    /// <summary />
+    [JsonPropertyName( "message" )]
+    public string Message { get; set; } = default!;
+
+    /// <summary />
+    /// <remarks>TODO: What are the possible values?</remarks>
+    [JsonPropertyName( "subType" )]
+    public string SubType { get; set; } = default!;
+
+    /// <summary />
+    /// <remarks>TODO: What are the possible values?</remarks>
+    [JsonPropertyName( "type" )]
+    public string Type { get; set; } = default!;
 }
 
 
