85b12416494ee34357cf9ce53d4235921887ffff
Filipe Toscano <filipe.toscano@gmail.com>
2025-02-10 19:10:30 +0400

    Add handy methods for creating attachment

---
+++ tests/Resend.Tests/EmailAttachmentTests.cs
@@ -0,0 +1,78 @@
+namespace Resend.Tests;
+
+/// <summary />
+public class EmailAttachmentTests
+{
+    /// <summary />
+    [Fact]
+    public void FromOk()
+    {
+        var path = Path.Combine( AppContext.BaseDirectory, @"..\\..\\..\\", "EmailAttachmentTests.cs" );
+        var size = new FileInfo( path ).Length;
+
+        var attach = EmailAttachment.From( path );
+
+
+        /*
+         * 
+         */
+        Assert.Equal( "EmailAttachmentTests.cs", attach.Filename );
+        Assert.Null( attach.Path );
+        Assert.NotNull( attach.Content );
+        Assert.Equal( size, attach.Content.LongLength );
+    }
+
+
+    /// <summary />
+    [Fact]
+    public void FromNotFound()
+    {
+        var path = Path.Combine( AppContext.BaseDirectory, @"..\\..\\..\\", "NOTFOUND.cs" );
+
+        Action act = () => EmailAttachment.From( path );
+
+
+        /*
+         * 
+         */
+        var ex = Assert.Throws<InvalidOperationException>( act );
+        Assert.StartsWith( "EA001:", ex.Message );
+    }
+
+
+    /// <summary />
+    [Fact]
+    public async Task FromAsyncOk()
+    {
+        var path = Path.Combine( AppContext.BaseDirectory, @"..\\..\\..\\", "EmailAttachmentTests.cs" );
+        var size = new FileInfo( path ).Length;
+
+        var attach = await EmailAttachment.FromAsync( path );
+
+
+        /*
+         * 
+         */
+        Assert.Equal( "EmailAttachmentTests.cs", attach.Filename );
+        Assert.Null( attach.Path );
+        Assert.NotNull( attach.Content );
+        Assert.Equal( size, attach.Content.LongLength );
+    }
+
+
+    /// <summary />
+    [Fact]
+    public void FromAsyncNotFound()
+    {
+        var path = Path.Combine( AppContext.BaseDirectory, @"..\\..\\..\\", "NOTFOUND.cs" );
+
+        Action act = () => EmailAttachment.FromAsync( path ).GetAwaiter().GetResult();
+
+
+        /*
+         * 
+         */
+        var ex = Assert.Throws<InvalidOperationException>( act );
+        Assert.StartsWith( "EA002:", ex.Message );
+    }
+}
