7365712c6329ae061d55f0e23f8d6650f0cc62c0
Filipe Toscano <filipe.toscano@gmail.com>
2025-02-28 23:30:41 +0400

    Implement IEquatable, so that we can call .Distinct() on lists

--- tests/Resend.Tests/EmailAddressTests.cs
+++ tests/Resend.Tests/EmailAddressTests.cs
@@ -25,4 +25,20 @@ public class EmailAddressTests
         Assert.Equal( "dev@example.com", src.Email );
         Assert.Equal( "John Doe", src.DisplayName );
     }
+
+
+    /// <summary />
+    [Fact]
+    public void Distinct()
+    {
+        EmailAddress src1 = "John Doe <dev@example.com>";
+        EmailAddress src2 = "dev@example.com";
+        EmailAddress src3 = "Johnny Doe <dev@example.com>";
+        EmailAddress src4 = "DEV@example.com";
+
+        var list = new List<EmailAddress>() { src1, src2, src3, src4 };
+        var uq = list.Distinct().ToList();
+
+        Assert.Equal( 2, uq.Count() );
+    }
 }
