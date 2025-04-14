e55993d5b1c4a652b318c6b16c4ec064325ea58e
Filipe Toscano <filipe.toscano@gmail.com>
2025-04-14 20:49:32 +0400

    Remove unnecessary async/await on outer call

--- src/Resend/ResendClient.cs
+++ src/Resend/ResendClient.cs
@@ -68,13 +68,13 @@ public class ResendClient : IResend
 
 
     /// <inheritdoc />
-    public async Task<ResendResponse<Guid>> EmailSendAsync( IIdempotencyKey idempotencyKey, EmailMessage email, CancellationToken cancellationToken = default )
+    public Task<ResendResponse<Guid>> EmailSendAsync( IIdempotencyKey idempotencyKey, EmailMessage email, CancellationToken cancellationToken = default )
     {
         var req = new HttpRequestMessage( HttpMethod.Post, "/emails" );
         req.Content = JsonContent.Create( email );
         req.Headers.Add( "Idempotency-Key", idempotencyKey.ToKey() );
 
-        return await Execute<ObjectId, Guid>( req, ( x ) => x.Id, cancellationToken );
+        return Execute<ObjectId, Guid>( req, ( x ) => x.Id, cancellationToken );
     }
 
 
