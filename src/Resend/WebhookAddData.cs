f28d9f160b0ad5b8bcd0a8ce0332bd586a0f94d8
Pedro Dell'Olio <pedrohdellolio@gmail.com>
2025-10-30 13:38:17 -0300

    fix: Add XML documentation to more webhook class properties

--- src/Resend/WebhookAddData.cs
+++ src/Resend/WebhookAddData.cs
@@ -2,11 +2,20 @@ using System.Text.Json.Serialization;
 
 namespace Resend;
 
+/// <summary>
+/// Request object to create a webhook.
+/// </summary>
 public class WebhookAddData
 {
+    /// <summary>
+    /// Webhook endpoint URL.
+    /// </summary>
     [JsonPropertyName( "endpoint" )]
     public string EndpointUrl { get; set; } = default!;
 
+    /// <summary>
+    /// Supported webhook events.
+    /// </summary>
     [JsonPropertyName( "events" )]
     public List<WebhookEventType> Events { get; set; } = default!;
 }
--- src/Resend/WebhookUpdateData.cs
+++ src/Resend/WebhookUpdateData.cs
@@ -2,16 +2,28 @@ using System.Text.Json.Serialization;
 
 namespace Resend;
 
+/// <summary>
+/// Request object to update a webhook.
+/// </summary>
 public class WebhookUpdateData
 {
+    /// <summary>
+    /// Webhook endpoint URL.
+    /// </summary>
     [JsonPropertyName( "endpoint" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
     public string? EndpointUrl { get; set; }
 
+    /// <summary>
+    /// Supported webhook events.
+    /// </summary>
     [JsonPropertyName( "events" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
     public List<WebhookEventType>? Events { get; set; }
 
+    /// <summary>
+    /// Webhook status.
+    /// </summary>
     [JsonPropertyName( "status" )]
     [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
     public WebhookStatus? Status { get; set; }
